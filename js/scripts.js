844addc6b6078e06e9d8a920b5ed5125730f50bd
Joey3001 <josephnero111@gmail.com>
2020-10-21 14:16:57 -0700

    Practice with forms and console log
    Co-authored by: Svea Wade <svealinneawade@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  console.log("Script executing!");
+  $("#formOne").submit(function(event) {
+    console.log("Submit form successfully reached.");
+    const person1Input = $("input#person1").val();
+    const person2Input = $("input#person2").val();
+    console.log(person1Input);
+    const animalInput= $("input#animal").val();
+    const exclamationInput = $("input#exclamation").val();
+    const verbInput = $("input#verb").val();
+    const nounInput = $("input#noun").val();
+
+    $("person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
+    event.preventDefault()
+    $("#story").show();
+    console.log("Submit form successfully reached.");
+  });
+});
